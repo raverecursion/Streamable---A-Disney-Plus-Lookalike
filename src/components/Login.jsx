5fe7f6db25f71962f9390488dabebefea6d9679e
ROLAND KIRALY <43180177+toobig4u@users.noreply.github.com>
2021-05-18 17:36:17 +0200

    REMOVE: Cleanup duplicates / leftovers

--- src/components/Login.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styled from "styled-components";
-function Login() {
-  return <Container></Container>;
-}
-
-export default Login;
-
-const Container = styled.div``;
